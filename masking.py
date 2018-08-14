cb60dd8b2e48351d22d1048cf5d396166365a8fd
Slav <slawomir8mazur@gmail.com>
2018-08-14 09:24:35 +0200

    checking masking methods

--- masking.py
+++ masking.py
@@ -1,4 +1,13 @@
 import numpy as np
+#Bitwise operators:
+# & and
+# | or
+# ^ xor
+# ~ not
+
+#Fancy indexing -> using a mask -> variable[mask] -> mask is array of the same size as variable     #OWNDATA True
+#Slicing -> variable[:,::2]                                                                         #OWNDATA False
+#Fancy indexing creates a copy, slicing creates a view(operates on the same object
 
 var = np.arange(-30,80,10)
 print(var)
@@ -13,3 +22,8 @@ negatives = var.copy()
 negatives[negatives<0] = 0
 print(negatives)
 print(var)
+
+#np.nonzero(var) - return tuple with positions of nonzero values in var
+print(np.nonzero(mask))
+print(np.nonzero((var<-10) | (var>20)))
+print((var<-10) | (var>20))
